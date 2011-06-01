4158600a1433e7086380f0cda2bc3c5b6b9f9f5c
Giacomo Mariani <gia.giotti@gmail.com>
2011-06-01 12:55:47 +0200

    A first qtom qprocess try...

--- src/3rdparty/applications/qtom/qtom.cpp
+++ src/3rdparty/applications/qtom/qtom.cpp
@@ -1,11 +1,23 @@
 #include "qtom.h"
 #include "ui_qtom.h"
+#include <QProcess>
 
 QtOm::QtOm(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::QtOm)
 {
     ui->setupUi(this);
+
+    QObject *parent;
+      ...
+    QString program = "./path/to/om";
+    QProcess *myProcess = new QProcess(parent);
+
+    QStringList arguments;
+    arguments << "battery" << "energy";
+    myProcess->start(program, arguments);
+    myProcess->read(int value);
+    connect(ui->battery_chargerLimit, SIGNAL(valueChanged(int)),    ui->battery_chargerLimitValue,  SLOT(setValue(int)));
 
     connect(ui->battery_chargerLimit, SIGNAL(valueChanged(int)),    ui->battery_chargerLimitValue,  SLOT(setValue(int)));
 }
