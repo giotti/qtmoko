61446fa0024b7f520fcce5c880f8a47db05782c4
Giacomo Mariani <gia.giotti@gmail.com>
2011-06-07 17:35:28 +0200

    Some semi-working mod.

--- src/3rdparty/applications/qtom/qtom.cpp
+++ src/3rdparty/applications/qtom/qtom.cpp
@@ -1,28 +1,34 @@
-#include "qtom.h"
-#include "ui_qtom.h"
+#include "./qtom.h"
+#include "./ui_qtom.h"
 #include <QProcess>
 
-QtOm::QtOm(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::QtOm)
-{
+QtOm::QtOm(QWidget *parent)
+: QWidget(parent),
+    ui(new Ui::QtOm) {
     ui->setupUi(this);
 
-    QObject *parent;
-      ...
-    QString program = "./path/to/om";
-    QProcess *myProcess = new QProcess(parent);
+    connect(ui->battery_chargerLimit, SIGNAL(valueChanged(int)),
+ ui->battery_chargerLimitValue,  SLOT(setValue(int)));
+    connect(ui->battery_chargerLimitValue, SIGNAL(valueChanged(int)),
+ ui->battery_chargerLimit,  SLOT(setValue(int)));
+
+    QObject *obj_parent = new QObject();
+
+    QString program = "/home/jack/GIT/qtmoko/src/3rdparty/applications/qtom-build-desktop/tmp_om/om";
+    QProcess *myProcess = new QProcess(obj_parent);
 
     QStringList arguments;
     arguments << "battery" << "energy";
     myProcess->start(program, arguments);
-    myProcess->read(int value);
-    connect(ui->battery_chargerLimit, SIGNAL(valueChanged(int)),    ui->battery_chargerLimitValue,  SLOT(setValue(int)));
 
-    connect(ui->battery_chargerLimit, SIGNAL(valueChanged(int)),    ui->battery_chargerLimitValue,  SLOT(setValue(int)));
+    int value;// = 100;
+    myProcess->read(value);
+
+    ui->battery_chargerLimitValue->setValue(value);
 }
 
-QtOm::~QtOm()
-{
+QtOm::~QtOm() {
+    //myProcess->finish();
     delete ui;
 }
+
--- src/3rdparty/applications/qtom/qtom.pro
+++ src/3rdparty/applications/qtom/qtom.pro
@@ -6,7 +6,7 @@
 
 QT       += core gui
 
-TARGET = OmProject
+TARGET = qtom
 TEMPLATE = app
 
 
