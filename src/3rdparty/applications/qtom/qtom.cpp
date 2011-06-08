d0087992fff7ca2fe00ac6f5fa571193dd8eecab
Giacomo Mariani <gia.giotti@gmail.com>
2011-06-08 17:18:04 +0200

    Some reading update.

--- src/3rdparty/applications/qtom/qtom.cpp
+++ src/3rdparty/applications/qtom/qtom.cpp
@@ -1,34 +1,78 @@
 #include "./qtom.h"
 #include "./ui_qtom.h"
-#include <QProcess>
+
+//------
+MyProcess::MyProcess( QObject* )
+: QProcess()
+{
+    _item = NULL;
+}
+
+MyProcess::~MyProcess() {}
+
+//------
+
+
 
 QtOm::QtOm(QWidget *parent)
 : QWidget(parent),
     ui(new Ui::QtOm) {
     ui->setupUi(this);
+    program = "/home/jack/GIT/qtmoko/src/3rdparty/applications/qtom/bin/om";
 
-    connect(ui->battery_chargerLimit, SIGNAL(valueChanged(int)),
- ui->battery_chargerLimitValue,  SLOT(setValue(int)));
-    connect(ui->battery_chargerLimitValue, SIGNAL(valueChanged(int)),
- ui->battery_chargerLimit,  SLOT(setValue(int)));
-
-    QObject *obj_parent = new QObject();
-
-    QString program = "/home/jack/GIT/qtmoko/src/3rdparty/applications/qtom-build-desktop/tmp_om/om";
-    QProcess *myProcess = new QProcess(obj_parent);
+    //--- connections ---
+    connect(ui->battery_chargerLimit, SIGNAL(valueChanged(int)), ui->battery_chargerLimitValue,  SLOT(setValue(int)));
+    connect(ui->battery_chargerLimitValue, SIGNAL(valueChanged(int)), ui->battery_chargerLimit,  SLOT(setValue(int)));
 
     QStringList arguments;
+
+    //--- Battery ---
     arguments << "battery" << "energy";
-    myProcess->start(program, arguments);
+    initProcess( ui->battery_energy, arguments );
+    arguments << "battery" << "charger-limit";
+    initProcess( ui->battery_chargerLimitValue, arguments );
+    arguments << "battery" << "temperature";
+    initProcess( ui->battery_temperature, arguments );
+    arguments << "battery" << "consumption";
+    initProcess( ui->battery_consumption, arguments );
+}
 
-    int value;// = 100;
-    myProcess->read(value);
+void QtOm::readOmOutput() {
+    MyProcess* proc = dynamic_cast<MyProcess*>(sender());
 
-    ui->battery_chargerLimitValue->setValue(value);
+    QByteArray myStdOut = proc->readAllStandardOutput();
+    QString val(myStdOut);
+
+    if( !proc->getAssociatedItem() )
+        return;
+
+    proc->getAssociatedItem()->setValue(val.toInt());
+}
+
+void QtOm::writeOmOutput() {
+    MyProcess* proc = dynamic_cast<MyProcess*>(sender());
+
+    QByteArray myStdOut = proc->readAllStandardOutput();
+    QString val(myStdOut);
+
+    if( !proc->getAssociatedItem() )
+        return;
+
+    proc->getAssociatedItem()->setValue(val.toInt());
+}
+
+void QtOm::initProcess( QSpinBox* sb, QStringList args ) {
+    //TODO change absolute path with something smarter...
+
+
+    MyProcess* p = new MyProcess();
+    p->setAssociatedItem(sb);
+    connect(p, SIGNAL(finished(int)), this, SLOT(readOmOutput()));
+
+    //launch om
+    p->start(program, args);
 }
 
 QtOm::~QtOm() {
-    //myProcess->finish();
     delete ui;
 }
-
--- src/3rdparty/applications/qtom/qtom.h
+++ src/3rdparty/applications/qtom/qtom.h
@@ -2,6 +2,8 @@
 #define QTOM_H
 
 #include <QWidget>
+#include <QProcess>
+#include <QSpinBox>
 
 namespace Ui {
     class QtOm;
@@ -14,9 +16,37 @@ class QtOm : public QWidget
 public:
     explicit QtOm(QWidget *parent = 0);
     ~QtOm();
+    QString program;
+
+
+protected:
+    virtual void initProcess( QSpinBox* sb, QStringList arguments );
+
+protected slots:
+    virtual void readOmOutput();
+    virtual void writeOmOutput();
 
 private:
     Ui::QtOm *ui;
+};
+
+
+/**
+  Initialize a custom process which store associated item pointer
+*/
+class MyProcess : public QProcess
+{
+    Q_OBJECT
+
+public:
+    explicit MyProcess(QObject *parent = 0);
+    ~MyProcess();
+
+    void setAssociatedItem( QSpinBox* sb ) { _item = sb; };
+    QSpinBox* getAssociatedItem() { return _item; };
+
+private:
+    QSpinBox* _item; //associated item
 };
 
 #endif // QTOM_H
